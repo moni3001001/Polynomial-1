6052090f30ca3a2b59c2da24c72e60925ae8e0da
Rajesh <srajesh@paypalcorp.com>
2016-08-05 08:58:30 +0530

    added

---
+++ Polynomial/src/Polynomial.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class Polynomial {
+
+	
+	private List<Double> coefficients;
+	
+	public Polynomial(String A){
+		
+	}
+	public Polynomial add(Polynomial A, Polynomial B){
+		
+		return null;
+	}
+	
+	public Polynomial  subtract(Polynomial A, Polynomial B){
+		
+		return null;
+	}
+	
+	public Polynomial multiply(Polynomial A, Polynomial B){
+		
+		return null;
+	}
+	
+	public String toString(Polynomial A){
+		return null;
+	}
+	
+	public String toHTML(Polynomial A){
+		return null;
+	}
+	
+	public String toLatex(Polynomial A){
+		return null;
+	}
+	
+}
